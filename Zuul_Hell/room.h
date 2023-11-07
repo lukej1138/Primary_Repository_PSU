8eaeb7169b311aa7f57889771b4b1b1e40e7b8d6
Lucas <lucasjjohnson@icloud.com>
2023-11-07 11:08:05 -0800

    here's the .h

--- Zuul_Hell/room.h
+++ Zuul_Hell/room.h
@@ -9,14 +9,15 @@ using namespace std;
 class room {
  public:
   room(char* name, map<int, char*>* rmItems, int id, map<char*, int>* exits, char* rmdescription);
-  void printItems(map<int, char*>* items);
+  void printItems();
 
   int getId();
 
-  void printExits(map<char*, int>* exits, vector<room*> rooms);
+  void printExits(vector<room*> rooms);
 
   char* getRmdescription();
 
+  //Used for adding items
   void setItems(char* item, int doodad, map<int, char*>* items, int location);
 
   void setExits(map<char*, int>* mp);
